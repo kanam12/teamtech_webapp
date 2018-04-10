3cd3c002c1f28be3d59b0665a82148d04f4dcc24
Kana Mishra <mishrakana@gmail.com>
2018-04-09 19:10:29 -0700

    multiple versions of webapps

---
+++ example/app.R
@@ -0,0 +1,97 @@
+library(shiny)
+library(tidyverse)
+
+# Define UI for dataset viewer app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Shiny Text"),
+  
+  # Sidebar layout with a input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for choosing dataset ----
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("rock", "pressure", "cars")),
+      
+      # Input: Numeric entry for number of obs to view ----
+      numericInput(inputId = "obs",
+                   label = "Number of observations to view:",
+                   value = 10)
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      verbatimTextOutput("print"),
+      
+      # plotOutput("histogram"),
+      
+      plotOutput("mpgPlot"),
+      
+      # Output: Verbatim text for data summary ----
+      verbatimTextOutput("summary"),
+      
+      # Output: HTML table with requested number of observations ----
+      tableOutput("view")
+      
+    )
+  )
+)
+
+# Define server logic to summarize and view selected dataset ----
+server <- function(input, output) {
+  
+  # Return the requested dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = read_csv(file = "../test.csv"),
+           "pressure" = pressure,
+           "cars" = cars)
+  })
+  
+  # Generate a summary of the dataset ----
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  output$print <- renderPrint({
+    dataset <- datasetInput()
+    x <- strtoi(dataset[1])
+    y <- strtoi(dataset[2])
+    hist(dataset$x, 
+         main = 'Pls Work' )
+    # typeof(dataset)
+    # dataset[2]
+    # renderPrint("hello")
+    # renderPrint(dataset)
+    # renderPrint("world")
+    # renderPrint(y)
+    # hist(x)
+    # plot(x = x, y = y, xlim = c(0, 20), ylim = c(0, 20))
+  })
+  
+  # Show the first "n" observations ----
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+    # casted <- strtoi(datasetInput())
+    # renderPrint("temp")
+    # renderPrint(casted)
+  })
+  
+  output$mpgPlot <- renderPlot({
+    plot(datasetInput())
+  })
+  
+  # output$histogram <- renderPlot({
+  #   hist(strtoi(datasetInput()[1]))
+  # })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
--- teamtech/app.R
+++ teamtech/app.R
@@ -7,8 +7,13 @@
 #    http://shiny.rstudio.com/
 #
 
+library(UsingR)
 library(shiny)
-library(datasets)
+library(Hmisc)
+library(corrplot)
+
+wd <- getwd()
+setwd(wd)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
@@ -40,7 +45,19 @@ ui <- shinyUI(fluidPage(
                  
                ),
                mainPanel(
-                 tableOutput('contents')
+                 h3('Output Information'),
+                 h4('File entered'),
+                 p("testing"),
+                 url <- a("Google Homepage", href="https://www.google.com/"),
+                 # p(url),
+                 verbatimTextOutput("helloworld"),
+                 verbatimTextOutput("ofile"),
+                 h4('You selected plot type'),
+                 verbatimTextOutput("oplotType"),
+                 h4('You entered'),
+                 verbatimTextOutput("odate"),
+                 plotOutput('newHist')
+                 # tableOutput('contents')
                )
              )
     ),
@@ -66,17 +83,38 @@ ui <- shinyUI(fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  
+   #print(input);
+   #print(output);
+  output$ofile        <- renderPrint({input$infile})
+  output$oplotType    <- renderPrint({input$plotType})
+  output$odate        <- renderPrint({input$date})
+  output$helloworld   <- renderPrint("hello world")
+  
+  plotdata <- reactive({
+    filestr <- input$infile
+    read.csv(filestr$name)
+  })
+  
+  output$newHist <- renderPlot({
+    hist(plotdata())
+  })
+  #   Conditional plot selection is test in progress
+      # corrdf <- cor(plotdata)
+      # output$newHist <- renderPlot({
+      #     corrplot(corrdf, method = "circle")
+      # })
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
+      x    <- faithful[, 2]
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
+
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 }
 
 # Run the application 
+renderPrint("!!hello world")
 shinyApp(ui = ui, server = server)
 
